37b54e8944b8f6b6353f63be8f400854e4b0ee6d
sanjaycoder01 <sjkulkarni31@gmail.com>
2025-10-26 13:52:27 +0530

    mongodb connection method

--- src/app.js
+++ src/app.js
@@ -1,103 +1,39 @@
 const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
-const {adminauth,userauth}=require("./middlewares/adminauth");
-// app.use("/", (req, res) => {
-//   console.log("Home");
-//   res.send("Home");
-// });
-// app.use("/home", (req, res) => {
-//   console.log("Hello World");
-// res.send("Hello World");
-// });
-// app.use("/home/2", (req, res) => {
-//     console.log("Home 2");
-//     res.send("Home 2");
-// });
 
-// app.use("/about", (req, res) => {
-//     console.log("About");
-//     res.send("About");
-// });
-// app.use("/contact", (req, res) => {
-//     console.log("Contact");
-//     res.send("Contact");
-// });
+// Import middleware
+const { adminauth } = require("./middlewares/adminauth");
 
+// Use middleware
+app.use("/", adminauth);
 
-
-//middleware chain
-// app.get("/info", (req, res,next) => {
-//   console.log("Info 1");
-//     res.send("Info1");
-//     next();
-// },(req, res) => {
-//   console.log("Info 2");
-//     res.send("Info 2");
-// });
-
-
-//multiple routes
-// app.get("/info", (req, res,next) => {
-//     res.send("Info");
-//     next();
-// });
-// //tHis is called route handler
-// app.get("/info1", (req, res,next) => {
-//     res.send("Info1");
-//     next();
-// });
-
-
-
-
-
-
-// app.get("/", (req, res) => {
-//     console.log("Home");
-//     res.send("Home");
-// });
-// // http://localhost:3000/about?userId=101&name=sabnjay
-// app.get("/about", (req, res) => {
-//   console.log(req.query);
-// res.send({name: "John", age: 20})
-// });
-// // http://localhost:3000/about/101/abc
-// app.get("/about/:userId/:name", (req, res) => {
-//     console.log(req.params);
-//     res.send("User ID: " + req.params.userId + " User Name: " + req.params.name);
-// });
-// app.post("/contact", (req, res) => {
-//     res.send("data sent to the server");
-// });
-// app.get("/contact", (req, res) => {
-//     console.log("Contact");
-//     res.send("Contact");
-// });
-
-app.use("/admin",adminauth);
-
+// Define routes
 app.get("/admin/getAllData", (req, res) => {
   res.send("All Data");
 });
 
-app.get("/user/getAllData",userauth, (req, res) => {
-  res.send("All Data");
-});
-app.get("/test",(req,res,next)=>{
-  throw new Error("This is a test error");
-});
-
-
-
-app.use("/",(err,req,res,next)=>{
-  if(err){
-  res.status(500).send("Internal Server Error");
+// Start server only after database connection
+const startServer = async () => {
+  try {
+    // Wait for database connection
+    const dbConnected = await connectDB();
+    
+    if (dbConnected) {
+      // Start the server only if database is connected
+      app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+        console.log("Database connection established successfully");
+      });
+    } else {
+      console.error("Failed to connect to database. Server not started.");
+      process.exit(1); // Exit the process if database connection fails
+    }
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1);
   }
-});
+};
 
-
-
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+// Start the application
+startServer();
