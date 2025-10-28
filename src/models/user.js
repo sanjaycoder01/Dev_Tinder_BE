028938a56e59a7d290bda533ec9a7bea7a4e9563
sanjaycoder01 <sjkulkarni31@gmail.com>
2025-10-28 10:15:28 +0530

    Schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,40 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const userSchema=new mongoose.Schema({
-  name:String,
-  email:String,
-  password:String,
-  age:Number,
-  gender:String,
-  location:String,
-});
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: Number,
+  gender: {
+    type: String,
+    validate(value) {
+      if (value !== "Male" && value !== "Female" && value !== "Other") {
+        throw new Error("Gender must be Male, Female or Other");
+      }
+    }
+  },
+  location: String,
+  skills: {
+    type: [String],
+  },
+  about: {
+    type: String,
+    default: "hi this is deafult value"
+  }
+}, { timestamps: true });
 
-const User=mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports=User;
+module.exports = User;
